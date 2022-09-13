a7489894c50b323126f99e4477fc76dfbeded46f
tsubasa_nagoya_u <tsubasa.kato.2000@gmail.com>
2022-09-14 01:08:47 +0900

    非表示機能の追加

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,17 +6,28 @@ export const TodoList = ({taskList,setTaskList}) => {
     setTaskList(taskList.filter((task) => task.id !== id));
   }
  
+  const handleDone = (id) => {
+    setTaskList(taskList.map((task) =>{
+        if(id === task.id){
+            return {
+                ...task,
+                completed: !task.completed
+            };
+        }
+        return task;
+    }))
+  };
   return (
     <div className="todoList">
         <div className='todos'>
             {taskList.map((task, index) => (
-                <div className='todo' key={index}>
+                <div className={`todo ${task.completed ? "completed" : ""}`} key={index}>
                 <div className='todoText'>
                     <span>{task.text}</span>
                 </div>
             
                 <div className="icons">
-                    <button>
+                    <button onClick={() => handleDone(task.id)}>
                     <i className="fa-solid fa-check"></i>
                     </button>
                     <button onClick={() => handleDelete(task.id)}>
