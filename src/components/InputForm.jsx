eb113b227d259c2ccbd4517cbf65fb3fc1cbeccb
tsubasa_nagoya_u <tsubasa.kato.2000@gmail.com>
2022-09-14 00:47:37 +0900

    検索を一覧表示

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+export const InputForm = ({taskList,setTaskList}) => {
+
+  const [inputText,setInputText]=useState("");
+  const handleSubmit=(e) => {
+    e.preventDefault();
+    //タスクの追加
+    setTaskList([
+        ...taskList,
+        {
+            text: inputText
+        }
+    ]);
+    //console.log(taskList);
+    setInputText("")
+  }
+    
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  }
+
+  return (
+    <div className="inputForm">
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleChange} value={inputText}/>
+            <button>
+            <i className="fa-regular fa-square-plus"></i>
+            </button>
+        </form>
+    </div>
+  )
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const TodoList = ({taskList,setTaskList}) => {
+  return (
+    <div className="todoList">
+        <div className='todos'>
+            {taskList.map((task, index) => (
+                <div className='todo' key={index}>
+                <div className='todoText'>
+                    <span>{task.text}</span>
+                </div>
+            
+                <div className="icons">
+                    <button>
+                    <i className="fa-solid fa-check"></i>
+                    </button>
+                    <button>
+                    <i className="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+            </div>
+            ))}
+            
+            
+        </div>
+    </div>
+  )
+}
