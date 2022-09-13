839a2cd80c4d33f5a5ce4402b089679148d4ed7a
tsubasa_nagoya_u <tsubasa.kato.2000@gmail.com>
2022-09-14 00:55:16 +0900

    削除機能の追加

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -9,6 +9,7 @@ export const InputForm = ({taskList,setTaskList}) => {
     setTaskList([
         ...taskList,
         {
+            id: taskList.length,
             text: inputText
         }
     ]);
